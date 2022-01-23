ee5598edc79ca1b8ae9612888047d2886d86442c
liaoyajing <liao-yajing@outlook.com>
2022-01-23 19:43:48 +0800

    init

---
+++ include/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#define seekerLogger_VERSION_MAJOR 
+#define seekerLogger_VERSION_MINOR 
+
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#define LOG_FILE_NAME "appliaction.log"
+#define LOG_USE_ASYN
+#define LOG_THREAD_COUNT 1
